8654b2285d7673cf2dd3959abbe2238558802009
xuzhenweiy <64451521+xuzhenweiy@users.noreply.github.com>
2021-02-20 23:18:47 +0800

    Update demo2.py

--- wenjianjia1/demo2.py
+++ wenjianjia1/demo2.py
@@ -1,2 +1,2 @@
-warning: Pulling without specifying how to reconcile divergent branches is
+print("xuzhenwei add demo2")warning: Pulling without specifying how to reconcile divergent branches is
 discouraged. You can squelch this message by running one of the following
